e953a40a945beab9220d17dce2cbc63a6cbe1e64
xiongxinxin_cd <xiongxinxin_cd@keruyun.com>
2018-10-29 18:39:51 +0800

    init classname-melt

---
+++ RADEME.md
@@ -0,0 +1,31 @@
+# Description
+A tool to generate a object which key-value is a string of class names based on given config.
+
+
+# input
+```
+{
+  hello:{
+   test: ['&'],
+   world: ['&', 'test1', '&-test2',{
+     child: ['&']
+   },]
+  }
+}
+```
+
+# output
+
+```
+ {
+   hello: {
+    _world_: 'hello-world test1 hello-world-test2',
+    world:{
+      _child: 'hello-worll-child'
+    },
+    _test: 'hello-test',
+   }
+  }
+```
+
+
---
+++ index.js
@@ -0,0 +1,94 @@
+const _ = require('lodash');
+
+/**
+ *
+ * @param {Object} skull
+ * @description return a compiled Object whose key-value consist of ancestor keys
+ * @example
+ * in:
+ * {hello:{
+ *  world: ['&'],
+ *  test: ['&', {
+ *    child: ['&']
+ *  }, 'test1', '&-test2']
+ * }}
+ * return:
+ * {
+ *  hello: {
+ *   world: 'hello-world test1 hello-world-test2',
+ *   test:{
+ *    '&': 'hello-test ',
+ *    'child': 'hello-test-child',
+ *   }
+ *  }
+ * }
+ */
+
+const compileSpecialChar = ( prefix, str) => {
+  return str.replace(/&/g, prefix );
+}
+
+const getCompiledWithChild = (pre, next, child) => {
+  if(_.isString(child)){
+    return {
+      ...pre,
+      [`_${next}`]: child,
+    }
+  }
+  return {
+    ...pre,
+    [next]: child,
+  };
+}
+
+const getDashString = strArr => {
+  return strArr.filter(str=> !!str).join('-');
+}
+
+
+
+const melt = skull => {
+  const noop = (init, parentKey) => {
+    if (!_.isObject(init)) {
+      throw new Error('Accept data should be type of Object.');
+    }
+
+    return Object.keys(init).reduce((pre, key) => {
+      const subItem = init[key];
+
+      if (_.isObject(subItem) && !subItem.length) {
+        const child = noop(subItem, getDashString([parentKey, key]));
+
+        return getCompiledWithChild(pre, key, child);
+      } else if (_.isArray(subItem)) {
+        return subItem.reduce((preSub, nextChild) => {
+          if (_.isString(nextChild)) {
+            // get current classnames 
+            const _key = `_${key}`;
+            console.log(parentKey)
+            const classnames = `${preSub[_key] || ''} ${compileSpecialChar(
+              getDashString([parentKey, key]),
+              nextChild,
+            )}`;
+            const childWithClassNames = {
+              self: classnames,
+            };
+
+            return getCompiledWithChild(preSub, key, classnames);
+         
+          } else if (_.isObject(nextChild)) {
+            // depth into child            
+            const child = noop(nextChild, getDashString([parentKey, key]));
+
+            return getCompiledWithChild(preSub, key, child);
+          }
+        }, {});
+      }
+    }, {});
+  };
+  return noop(skull, '');
+};
+
+module.exports = {
+  melt,
+}
